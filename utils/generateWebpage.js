6c2b3804f31e39fd0d5eea384eda507a5e841197
marchetype <114883447+marchetype@users.noreply.github.com>
2023-01-09 15:06:41 -0500

    Added new constructors for team member objects, added a test HTML file in order to preview layout of the site.

---
+++ utils/generateWebpage.js
@@ -0,0 +1,6 @@
+function generateMarkdown(data) {
+//section below will utilize the code created in testHTML file. Until that is complete, it will remain blank on this file.   
+    return `
+
+    `
+}

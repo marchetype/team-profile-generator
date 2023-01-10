1f8a21a9123984b7140afdc5640a1a3c87d2d55c
marchetype <114883447+marchetype@users.noreply.github.com>
2023-01-10 12:33:53 -0500

    Added directories and test files

--- utils/generateWebpage.js
+++
@@ -1,6 +0,0 @@
-function generateMarkdown(data) {
-//section below will utilize the code created in testHTML file. Until that is complete, it will remain blank on this file.   
-    return `
-
-    `
-}

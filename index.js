ecf9de3e6bfde0ddd2db7194e6e4714f2c780f84
marchetype <114883447+marchetype@users.noreply.github.com>
2023-01-10 12:59:19 -0500

    reformatted javascript code to define all classes in seperate files within the 'lib' directory

--- index.js
+++ index.js
@@ -1,25 +1,2 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateWebpage = require('./utils/generateWebpage');
-
-function Team_manager(name, id, email, office_num) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-    this.office_num = office_num;
-}
-
-function Engineer(name, id, email, github) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-    this.github = github;
-}
-
-function Intern(name, id, email, school) {
-    this.name = name;
-    this.id = id;
-    this.email = email;
-    this.school = school;
-
-}
